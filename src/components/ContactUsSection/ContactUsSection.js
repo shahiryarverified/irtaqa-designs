a4eb4ca306b948e5c7a70eb5e3e06865384d7fbd
muhammad2022sculptor <muhammad.shahiryar.sculptor@gmail.com>
2023-12-27 10:13:40 +0500

    Contact Us Section Under Dev

--- src/components/ContactUsSection/ContactUsSection.js
+++ src/components/ContactUsSection/ContactUsSection.js
@@ -4,22 +4,24 @@ import "./style.css"; // Make sure to create this CSS file
 function ContactUsSection() {
   return (
     <div className="contact-us-section">
-      <h2>Contact US</h2>
-      <div className="contact-info">
-        <h3>Contact</h3>
-        <p>Fill up the form below to contact</p>
-        <p className="phone-number">📞 +92-3009596802</p>
-        <p className="email">✉️ contact@irtaqadesigns</p>
-        <p className="address">📍 DHA4,Lahore,Pakistan</p>
-        <div className="social-icons">
-          {/* Icons would be inserted here, possibly using an icon library like Font Awesome */}
+      <div className="contact-us-section-inner">
+        <div className="contact-info">
+          <h2>Contact US</h2>
+          <h3>Contact</h3>
+          <p>Fill up the form below to contact</p>
+          <p className="phone-number">📞 +92-3009596802</p>
+          <p className="email">✉️ contact@irtaqadesigns</p>
+          <p className="address">📍 DHA4,Lahore,Pakistan</p>
+          <div className="social-icons">
+            {/* Icons would be inserted here, possibly using an icon library like Font Awesome */}
+          </div>
         </div>
-      </div>
-      <div className="contact-form">
-        <input type="text" placeholder="Your Name" />
-        <input type="email" placeholder="Mail" />
-        <textarea placeholder="Message"></textarea>
-        <button>Send Message</button>
+        <div className="contact-form">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Mail" />
+          <textarea placeholder="Message"></textarea>
+          <button>Send Message</button>
+        </div>
       </div>
     </div>
   );
