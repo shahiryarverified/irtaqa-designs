f2e3886826b62fa558e46e1de17a44469a9c2e84
muhammad2022sculptor <muhammad.shahiryar.sculptor@gmail.com>
2023-12-27 08:53:43 +0500

    Our Projects Basic Structure Designed

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -1,56 +1,99 @@
-import React from "react";
-import "./style.css"; // Import the CSS file for styling
+import React, { useState } from "react";
+import "./style.css";
+import image1 from "../../assets/projects/1.jpg";
+import image2 from "../../assets/projects/2.jpg";
+import image3 from "../../assets/projects/3.jpg";
+import image4 from "../../assets/projects/4.jpeg";
+import image5 from "../../assets/projects/5.jpeg";
+import image6 from "../../assets/projects/6.jpeg";
 
 const projects = [
   {
     name: "Gulbahar Plaza",
-    image: "path-to-gulbahar-plaza.jpg",
+    image: image1,
     type: "Commercial",
   },
   {
     name: "Commercial Mall Kharian",
-    image: "path-to-commercial-mall.jpg",
+    image: image2,
     type: "Commercial",
   },
   {
     name: "Meezan Bank Reginal Office",
-    image: "path-to-meezan-bank.jpg",
+    image: image3,
     type: "Commercial",
   },
   {
     name: "Leather Tennary, Sialkot",
-    image: "path-to-leather-tennary.jpg",
+    image: image4,
     type: "Residential",
   },
   {
     name: "Al Najran Avenue KSA",
-    image: "path-to-al-najran.jpg",
+    image: image5,
+    type: "Residential",
+  },
+  {
+    name: "Khan Villa, Lahore",
+    image: image6,
     type: "Residential",
   },
 ];
 
 const ProjectsSection = () => {
+  const [activeSection, setActiveSection] = useState("All");
+
   return (
     <div className="projects-section">
       <h2>Most Popular Projects</h2>
       <div className="filter-buttons">
-        <button>All</button>
-        <button>Commercial</button>
-        <button>Residential</button>
+        <button
+          className={
+            "filter-button " + (activeSection === "All" ? "active" : "")
+          }
+          onClick={() => {
+            setActiveSection("All");
+          }}
+        >
+          All
+        </button>
+        <button
+          className={
+            "filter-button " + (activeSection === "Commercial" ? "active" : "")
+          }
+          onClick={() => {
+            setActiveSection("Commercial");
+          }}
+        >
+          Commercial
+        </button>
+        <button
+          className={
+            "filter-button " + (activeSection === "Residential" ? "active" : "")
+          }
+          onClick={() => {
+            setActiveSection("Residential");
+          }}
+        >
+          Residential
+        </button>
       </div>
       <div className="projects-grid">
-        {projects.map((project, index) => (
-          <div className="project-card" key={index}>
-            <img
-              src={project.image}
-              alt={project.name}
-              className="project-image"
-            />
-            <div className="project-info">
-              <span className="project-name">{project.name}</span>
-            </div>
-          </div>
-        ))}
+        {projects.map(
+          (project, index) =>
+            (activeSection === "All" || project.type === activeSection) && (
+              <div className="project-card" key={index}>
+                <img
+                  src={project.image}
+                  alt={project.name}
+                  className="project-image"
+                />
+                <div className="project-info">
+                  <span className="project-name">{project.name}</span>
+                </div>
+              </div>
+            )
+        )}
       </div>
     </div>
   );
