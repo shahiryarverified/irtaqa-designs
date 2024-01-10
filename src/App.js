1acf047737a88386f64a206f29441e8be62d0393
muhammad2022sculptor <muhammad.shahiryar.sculptor@gmail.com>
2024-01-10 23:45:53 +0500

    Redeployed the whole website

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React from "react";
 import "./App.css";
-// import Header from "./components/Header/Header";
-// import Footer from "./components/Footer/Footer";
-// import NavBar from "./components/NavBar/NavBar";
-// import AboveHeader from "./components/AboveHeader/AboveHeader";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
+import AboveHeader from "./components/AboveHeader/AboveHeader";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-// import HomePage from "./pages/HomePage/HomePage";
-// import ContactUsSection from "./components/ContactUsSection/ContactUsSection";
-// import StorySection from "./components/Storysection/StorySection";
-// import ServicesSection from "./components/ServicesSection/ServicesSection";
-// import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
-// import TeamSection from "./components/TeamSection/TeamSection";
+import HomePage from "./pages/HomePage/HomePage";
+import ContactUsSection from "./components/ContactUsSection/ContactUsSection";
+import StorySection from "./components/Storysection/StorySection";
+import ServicesSection from "./components/ServicesSection/ServicesSection";
+import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
+import TeamSection from "./components/TeamSection/TeamSection";
 import NotFound from "./pages/NotFound/NotFound";
-// import Project from "./pages/Project/Project";
-// import Service from "./pages/Service/Service";
+import Project from "./pages/Project/Project";
+import Service from "./pages/Service/Service";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* <AboveHeader /> */}
-        {/* <Header /> */}
-        {/* <NavBar /> */}
+        <AboveHeader />
+        <Header />
+        <NavBar />
         <Routes>
-          {/* <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/" element={<HomePage />} />
           <Route exact path="/contact" element={<ContactUsSection />} />
           <Route exact path="/about" element={<StorySection />} />
           <Route exact path="/services" element={<ServicesSection />} />
           <Route exact path="/projects" element={<ProjectsSection />} />
           <Route exact path="/team" element={<TeamSection />} />
           <Route exact path="/project/:id" element={<Project />} />
-          <Route exact path="/service/:id" element={<Service />} /> */}
+          <Route exact path="/service/:id" element={<Service />} />
           <Route exact path="*" element={<NotFound />} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </Router>
   );
