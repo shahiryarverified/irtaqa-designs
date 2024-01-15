e108700ecb65019dfef5ef617cf983848b1aa8fa
muhammad2022sculptor <muhammad.shahiryar.sculptor@gmail.com>
2024-01-15 10:31:03 +0500

    Project Counter Finished

---
+++ src/components/ProjectCounter/ProjectCounter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./projectcounter.css";
+import {
+  CheckCircle,
+  EmojiEmotions,
+  LocationCity,
+  SentimentSatisfiedAlt,
+} from "@mui/icons-material";
+
+const ProjectCounter = () => {
+  return (
+    <div className="project-counter">
+      <div className="counter-section">
+        <CheckCircle style={{ fontSize: "7rem", paddingBottom: "30px" }} />
+        <h1>300+</h1>
+        <h2>Projects Completed</h2>
+      </div>
+      <div className="counter-section">
+        <EmojiEmotions style={{ fontSize: "7rem", paddingBottom: "30px" }} />
+        <h1>280+</h1>
+        <h2>Happy Clients</h2>
+      </div>
+      <div className="counter-section">
+        <LocationCity style={{ fontSize: "7rem", paddingBottom: "30px" }} />
+        <h1>20+</h1>
+        <h2>Cities</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCounter;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,11 +3,13 @@ import HeroSection from "../../components/HeroSection/HeroSection";
 import Services from "../../components/ServicesSection/ServicesSection";
 import ProjectsSection from "../../components/ProjectsSection/ProjectsSection";
 import OurClientsSection from "../../components/OurClientsSection/OurClientsSection";
+import ProjectCounter from "../../components/ProjectCounter/ProjectCounter";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection name="hero" />
+      <ProjectCounter name="counter" />
       <Services name="services" />
       <OurClientsSection name="clients" />
       <ProjectsSection name="projects" />
